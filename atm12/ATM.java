1f2dde7cff0ffeccab3b81d52b19815355ee3708
jahidjubaer <jahidjubaer17@gmail.com>
2023-11-26 11:25:38 +0600

    ATM Machine add by Jahid

---
+++ atm12/ATM.java
@@ -0,0 +1,17 @@
+
+package atm12;
+
+
+import java.text.DecimalFormat;
+import java.util.*;
+import java.io.IOException;
+
+public class ATM extends OptionMenu {
+        public static void main(String[] args) throws IOException {
+                OptionMenu optionMenu = new OptionMenu();
+                optionMenu.getLogin();
+        }
+}
+
+
+
